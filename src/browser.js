db5a5f2e0d67d04e87cf9d12b9992e24b43bff82
Mitsunee <michelle@mitsunee.com>
2021-03-21 15:49:03 +0100

    Reintroduce fetchJson module for browser version

--- src/browser.js
+++ src/browser.js
@@ -2,10 +2,12 @@ import range from "./modules/range";
 import clamp from "./modules/clamp";
 import timeout from "./modules/timeout";
 import interval from "./modules/interval";
+import fetchJson from "./modules/browser/fetchJson";
 
 export {
     range,
     clamp,
     timeout,
     interval,
+    fetchJson
 };
